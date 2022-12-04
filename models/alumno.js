0c66cfc1bc66ccad3d3b0237f5bb119cb07f3a94
fadedphoenix7 <pepelotas.jose@gmail.com>
2022-12-04 00:25:36 -0600

    Alummno model change

--- models/alumno.js
+++ models/alumno.js
@@ -1,10 +1,11 @@
 class Alumno{
-  constructor({id, nombres, apellidos, matricula, promedio}){
+  constructor({id, nombres, apellidos, matricula, promedio, fotoPerfilUrl}){
     this.id = id || 0;
     this.nombres = nombres;
     this.apellidos = apellidos;
     this.matricula = matricula;
     this.promedio = promedio;
+    this.fotoPerfilUrl = fotoPerfilUrl;
   }
 }
 
